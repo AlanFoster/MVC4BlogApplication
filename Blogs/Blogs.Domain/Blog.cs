e3387202fb01dd3b78ee753d6ad33e79f5e75c46
Alan Foster <alan@alanfoster.me>
2013-09-18 02:15:07 +0100

    Add initial domain datamodel

---
+++ Blogs/Blogs.Domain/Blog.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blogs.Domain
+{
+    public class Blog
+    {
+        public int Id { get; set; }
+        public User User { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+    }
+}
---
+++ Blogs/Blogs.Domain/IBlogDataSource.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blogs.Domain
+{
+    public interface IBlogDataSource
+    {
+        IQueryable<Blog> Blogs { get; }
+        IQueryable<User> Users { get; }
+    }
+}
